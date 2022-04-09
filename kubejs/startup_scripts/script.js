fe477257c54b8e028daef7db2032c62ffd82b380
TehFoxFire <20863498+TehFoxFire@users.noreply.github.com>
2022-04-09 16:22:29 -0500

    Changed JEI mod sorting and added pack icon and title.

--- kubejs/startup_scripts/script.js
+++ kubejs/startup_scripts/script.js
@@ -4,10 +4,10 @@ console.info('Hello, World! (You will only see this line once in console, during
 
 onEvent('item.registry', event => {
 	// Register new items here
-	 event.create('example_item').displayName('Example Item')
+	// event.create('example_item').displayName('Example Item')
 })
 
 onEvent('block.registry', event => {
 	// Register new blocks here
-	 event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
+	// event.create('example_block').material('wood').hardness(1.0).displayName('Example Block')
 })
