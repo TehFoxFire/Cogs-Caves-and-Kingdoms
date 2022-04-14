e8d1d63e6de74562daed0b59996a1167e4f2091a
TehFoxFire <20863498+TehFoxFire@users.noreply.github.com>
2022-04-14 09:49:31 -0500

    Removed Recipes for Enderbook, teleporter, and hid several other items fron JEI

--- kubejs/client_scripts/script.js
+++ kubejs/client_scripts/script.js
@@ -4,7 +4,6 @@ console.info('Hello, World! (You will see this line every time client resources 
 
 onEvent('jei.hide.items', event => {
 	// Hide items in JEI here
-	event.hide('minecraft:cobblestone')
 	event.hide('#itemfilters:filters')
 	event.hide(['castle_in_the_sky:red_door', 'castle_in_the_sky:blue_door', 'castle_in_the_sky:yellow_door', 'castle_in_the_sky:fake_beacon', Item.of('cyclic:ender_book', '{Damage:0}'), 'cyclic:teleport', Item.of('cyclic:sleeping_mat', '{Damage:0}')])
 })
--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -9,11 +9,16 @@ console.info('Hello, World! (You will see this line every time server resources 
 
 onEvent('recipes', event => {
 	// Change recipes here
+	event.remove({output: Item.of('cyclic:ender_book', '{Damage:0}')})
+	event.remove({output: 'cyclic:teleport'})
+	event.remove({output: Item.of('cyclic:sleeping_mat', '{Damage:0}')})
 })
 
 onEvent('item.tags', event => {
 	// Get the #forge:cobblestone tag collection and add Diamond Ore to it
 	// event.get('forge:cobblestone').add('minecraft:diamond_ore')
+	event.get('forge:ores').add('stalwart_dungeons:chorundum_ore')
+	event.get('forge:ores').add('stalwart_dungeons:tungsten_ore')
 
 	// Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
 	// event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
